e4a9e561f424bf51635d8e3af9d5e5a080171aa0
YilmazNL2005 <99069863@mydavinci.nl>
2023-03-08 10:30:34 +0100

    Taken

--- module_1_javaScriptCrashCourse/horeca.js
+++ module_1_javaScriptCrashCourse/horeca.js
@@ -1,4 +1,16 @@
-let bestellen = prompt("Wat wilt u bestellen?")
-if (bestellen != "fris", "bier", "wijn") {
-    alert("Dit ken ik niet.")
+
+let tempstopchecknotfalse = true
+while (tempstopchecknotfalse){
+    let bestellen = prompt("Wat wilt u bestellen? Keuze: fris, bier of wijn. Of liever stoppen? type: stop ").toLocaleLowerCase
+    if (bestellen != "fris" && bestellen != "bier" && bestellen != "wijn"){
+        alert("Dit ken ik niet.")
+    } else{
+        if (bestellen === "stop"){
+            tempstopchecknotfalse = false
+        }else{
+            let vraag_aantal = parseInt(prompt(`Hoeveel wil je van: ${bestellen} hebben?: `))
+        
+            
+        }
+    }
 }
