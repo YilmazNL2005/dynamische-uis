2c7f1ce00be5eb1fddcf0979449cbfb8333f83f2
YilmazNL2005 <99069863@mydavinci.nl>
2023-03-09 14:54:32 +0100

    End

--- module_1_javaScriptCrashCourse/horeca.js
+++ module_1_javaScriptCrashCourse/horeca.js
@@ -1,12 +1,11 @@
-
-let tempstopchecknotfalse = true
-while (tempstopchecknotfalse){
-    let bestellen = prompt("Wat wilt u bestellen? Keuze: fris, bier of wijn. Of liever stoppen? type: stop ").toLocaleLowerCase
-    if (bestellen != "fris" && bestellen != "bier" && bestellen != "wijn"){
+let nietfalse = true
+while (nietfalse){
+    let bestellen = prompt("Wat wilt u bestellen? Keuze: fris, bier of wijn. Of liever stoppen? type: stop ") //.toLocaleLowerCase
+    if (bestellen != "fris" && bestellen != "bier" && bestellen != "wijn" && bestellen != "stop"){
         alert("Dit ken ik niet.")
     } else{
         if (bestellen === "stop"){
-            tempstopchecknotfalse = false
+            nietfalse = false
         }else{
             let vraag_aantal = parseInt(prompt(`Hoeveel wil je van: ${bestellen} hebben?: `))
         
@@ -14,3 +13,4 @@ while (tempstopchecknotfalse){
         }
     }
 }
+getelementbyid.innerHTML=("hoeveelheid").innertext=vraag_aantal
