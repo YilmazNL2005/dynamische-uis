06bf0622461e5f6ad370cda6c2e3b67efc4bea57
YilmazNL2005 <99069863@mydavinci.nl>
2023-04-11 16:19:24 +0200

    end

--- module_1_javaScriptCrashCourse/horeca2.js
+++ module_1_javaScriptCrashCourse/horeca2.js
@@ -7,39 +7,34 @@ let prijs_wijn = 3.75
 const prijzen = [0, 0, 0] // fris, bier, wijn
 const aantal = [0, 0, 0] // 0: fris; 1: bier; 2: wijn;
 const gevraagde_product = []
+let producten = ["fris", "bier", "wijn"]
 
 while (doorBestellen){ // fris, bier of wijn. Anders stoppen typ stop.
     vraag = prompt("Wat wilt u bestellen? of stop? ");
         if(vraag === "stop"){doorBestellen = false;}
-        if (vraag != "fris" && vraag != "bier" && vraag != "wijn" && vraag != "stop"){
-            alert("je moet iets bestellen dat aanwezig is.");
-        } 
-        if (vraag === "fris"){
-            gevraagde_product[0] = vraag;
-            console.log(gevraagde_product)
-        }
-        if (vraag === "bier"){
-            gevraagde_product[1] = vraag;
-            console.log(gevraagde_product)
-        }
-        if (vraag === "wijn"){
-            gevraagde_product[2] = vraag;
-            console.log(gevraagde_product)
-        }
-        if (vraag === "fris" || vraag === "bier" || vraag === "wijn"){
+        else if (vraag in producten){
             let vraag_aantal = parseInt(prompt(`Hoeveel wil je van: ${vraag} hebben?: `));
             if (vraag === "fris"){
+                gevraagde_product[0] = vraag;
+                console.log(gevraagde_product)
                 aantal[0] += vraag_aantal;
                 console.log(aantal)
             }
             if (vraag === "bier"){
+                gevraagde_product[1] = vraag;
+                console.log(gevraagde_product)
                 aantal[1] += vraag_aantal;
                 console.log(aantal)
             }
             if (vraag === "wijn"){
+                gevraagde_product[2] = vraag;
+                console.log(gevraagde_product)
                 aantal[2] += vraag_aantal;
                 console.log(aantal)
             }
+        }
+        else {
+            alert("je moet iets bestellen dat aanwezig is.");
         }
                     }
 
@@ -50,13 +45,17 @@ let afronding_fris = prijzen[0].toFixed(2);
 let afronding_bier = prijzen[1].toFixed(2);
 let afronding_wijn = prijzen[2].toFixed(2);
 console.log(prijzen)
-// let num = 5.56789;
-// let n = num.toFixed(2);
+
+let totaal_bedrag = prijzen[0] + prijzen[1] + prijzen[2];
+let afronding_totaal = totaal_bedrag.toFixed(2);
+
 if ("fris" === gevraagde_product[0]){
-document.write(gevraagde_product[0]+" &nbsp; - &nbsp; Aantal &nbsp; "+ aantal[0] +" &nbsp; - &nbsp; Prijs &nbsp; € " +prijzen[0] + "<br>"); }
+document.write(gevraagde_product[0]+" &nbsp; - &nbsp; Aantal &nbsp; "+ aantal[0] +" &nbsp; - &nbsp; Prijs &nbsp; € " +afronding_fris + "<br>"); }
 
 if ("bier" === gevraagde_product[1]){
-document.write(gevraagde_product[1]+" &nbsp; - &nbsp; Aantal &nbsp; "+ aantal[1] +" &nbsp; - &nbsp; Prijs &nbsp; € " +prijzen[1] + "<br>"); }
+document.write(gevraagde_product[1]+" &nbsp; - &nbsp; Aantal &nbsp; "+ aantal[1] +" &nbsp; - &nbsp; Prijs &nbsp; € " +afronding_bier + "<br>"); }
 
 if ("wijn" === gevraagde_product[2]){
-document.write(gevraagde_product[2]+" &nbsp; - &nbsp; Aantal &nbsp; "+ aantal[2] +" &nbsp; - &nbsp; Prijs &nbsp; € " +prijzen[2] + "<br>"); }
+document.write(gevraagde_product[2]+" &nbsp; - &nbsp; Aantal &nbsp; "+ aantal[2] +" &nbsp; - &nbsp; Prijs &nbsp; € " +afronding_wijn + "<br>"); }
+
+document.write("Totaalbedrag : € " + afronding_totaal)
